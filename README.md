f02e046bdecff5864c60531cc5eb7f23d737f47c
Cliff Syner <c4syner@gmail.com>
2024-09-06 14:26:01 -0500

    feat: real btc data when deploying

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # contracts
+
+### Deploy
+```
+source .env && forge script --chain sepolia scripts/DeployRiftExchange.s.sol:DeployRiftExchange --rpc-url $SEPOLIA_RPC_URL --broadcast --sender $SEPOLIA_SENDER --private-key $SEPOLIA_PRIVATE_KEY --verify --etherscan-api-key $ETHERSCAN_API_KEY 
+```
+
--- scripts/DeployRiftExchange.s.sol
+++ scripts/DeployRiftExchange.s.sol
@@ -6,26 +6,98 @@ import "forge-std/console.sol";
 import "../src/RiftExchange.sol";
 
 contract DeployRiftExchange is Script {
+    function stringToUint(string memory s) internal pure returns (uint) {
+        bytes memory b = bytes(s);
+        uint result = 0;
+        for (uint i = 0; i < b.length; i++) {
+            uint c = uint(uint8(b[i]));
+            if (c >= 48 && c <= 57) {
+                result = result * 10 + (c - 48);
+            }
+        }
+        return result;
+    }
+    
+   function _substring(string memory _base, int _length, int _offset)
+        internal
+        pure 
+        returns (string memory) {
+        bytes memory _baseBytes = bytes(_base);
+
+        assert(uint(_offset + _length) <= _baseBytes.length);
+
+        string memory _tmp = new string(uint(_length));
+        bytes memory _tmpBytes = bytes(_tmp);
+
+        uint j = 0;
+        for (uint i = uint(_offset); i < uint(_offset + _length); i++) {
+            _tmpBytes[j++] = _baseBytes[i];
+        }
+
+        return string(_tmpBytes);
+    }
+
+  function fetchBlockHeight() public returns (uint256) {
+        // Prepare the curl command with jq
+        string[] memory curlInputs = new string[](3);
+        curlInputs[0] = "bash";
+        curlInputs[1] = "-c";
+        curlInputs[2] = string(abi.encodePacked(
+            "curl --data-binary '{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"getblockchaininfo\", \"params\": []}' ",
+            "-H 'content-type: text/plain;' -s ",
+            vm.envString("BITCOIN_RPC"),
+            " | jq -r '.result.blocks'"
+        ));
+        string memory _blockHeightStr = vm.toString(vm.ffi(curlInputs));
+        string memory blockHeightStr =  _substring(_blockHeightStr, int(bytes(_blockHeightStr).length)-2, 2);
+        uint256 blockHeight = stringToUint(blockHeightStr);
+        return blockHeight;
+  }
+
+    function fetchBlockHash(uint256 height) public returns (bytes32) {
+        string memory heightStr = vm.toString(height);
+        string[] memory curlInputs = new string[](3);
+        curlInputs[0] = "bash";
+        curlInputs[1] = "-c";
+        curlInputs[2] = string(abi.encodePacked(
+            "curl --data-binary '{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"getblockhash\", \"params\": [",
+            heightStr,
+            "]}' -H 'content-type: text/plain;' -s ",
+            vm.envString("BITCOIN_RPC"),
+            " | jq -r '.result'"
+        ));
+        bytes memory result = vm.ffi(curlInputs);
+        return bytes32(result);
+    }
+
+    function calculateRetargetHeight(uint256 height) public pure returns (uint256) {
+        uint256 retargetHeight = height - (height % 2016);
+        return retargetHeight;
+    }
+
     function run() external {
         vm.startBroadcast();
 
         console.log("Starting deployment...");
 
+        uint256 bitcoin_chain_height = fetchBlockHeight();
+        uint256 initialCheckpointHeight = bitcoin_chain_height - 6;
+        bytes32 initialBlockHash = fetchBlockHash(initialCheckpointHeight);
+        bytes32 initialRetargetBlockHash = fetchBlockHash(calculateRetargetHeight(initialCheckpointHeight - 2016));
+
         // Define the constructor arguments
-        uint256 initialCheckpointHeight = 0;
-        bytes32 initialBlockHash = bytes32(0);
-        bytes32 initialRetargetBlockHash = hex"00ca6cebffbb631e1dcb7588151f5cd92b1fd99c85e065030307de4c677b6dba";
-        address verifierContractAddress = address(0x01);
+        address verifierContractAddress = address(0x3B6041173B80E77f038f3F2C0f9744f04837185e);
         address depositTokenAddress = address(0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0);
         uint256 proverReward = 5 * 10 ** 6; // 5 USDT
         uint256 releaserReward = 2 * 10 ** 6; // 2 USDT
-        bytes32 verificationKeyHash = hex"00ca6cebffbb631e1dcb7588151f5cd92b1fd99c85e065030307de4c677b6dba";
-        address payable protocolAddress = payable(address(1));
+        bytes32 verificationKeyHash = hex"00bfe50d803b9b4aee4318e822079e5165b4413ecc9d81e194eeef3e3b4afeab";
+        address payable protocolAddress = payable(address(0x9FEEf1C10B8cD9Bc6c6B6B44ad96e07F805decaf));
         address owner = msg.sender;
 
         console.log("Deploying RiftExchange...");
         console.log("initialCheckpointHeight:", initialCheckpointHeight);
-        console.log("initialBlockHash:", uint256(initialBlockHash));
+        console.log("initialBlockHash:");
+        console.logBytes32(initialBlockHash);
         console.log("verifierContractAddress:", verifierContractAddress);
         console.log("depositTokenAddress:", depositTokenAddress);
         console.log("proverReward:", proverReward);
